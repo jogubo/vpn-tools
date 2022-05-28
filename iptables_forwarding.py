06bda49eb4ada367bfcc12103edb09e912f0b2b7
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-05-28 23:44:04 +0200

    clean code

--- iptables_forwarding.py
+++ iptables_forwarding.py
@@ -40,16 +40,12 @@ FROM_INTERNET_TO_VPN_CLIENT = {
 def cmd_iptables(ipv):
     if ipv == 'ipv4':
         return 'iptables'
-    elif ipv == 'ipv6':
+    if ipv == 'ipv6':
         return 'ip6tables'
-    else:
-        return None
 
 
 def routing_to_internet(ipv, port, protocol):
     iptables = cmd_iptables(ipv)
-    if iptables is None:
-        return
     cmd(
         f"{iptables} -t nat -A POSTROUTING -s \
             {VPN_CLIENT_IP[ipv]} -p {protocol} --dport {port} -j SNAT --to \
@@ -63,8 +59,6 @@ def routing_to_internet(ipv, port, protocol):
 
 def routing_to_vpn_client(ipv, port, protocol):
     iptables = cmd_iptables(ipv)
-    if iptables is None:
-        return
     cmd(
         f"{iptables} -t nat -A PREROUTING -i {INTERFACE} \
             -p {protocol} --dport {port} -j DNAT \
@@ -77,19 +71,20 @@ def routing_to_vpn_client(ipv, port, protocol):
 
 
 if __name__ == '__main__':
+
     for protocol in FROM_VPN_CLIENT_TO_INTERNET:
         for port in FROM_VPN_CLIENT_TO_INTERNET[protocol]:
             if PUBLIC_IP['ipv4']:
                 routing_to_internet('ipv4', port, protocol)
             if PUBLIC_IP['ipv6']:
                 routing_to_internet('ipv6', port, protocol)
+
     for protocol in FROM_INTERNET_TO_VPN_CLIENT:
         for port in FROM_INTERNET_TO_VPN_CLIENT[protocol]:
             if PUBLIC_IP['ipv4']:
                 routing_to_vpn_client('ipv4', port, protocol)
             if PUBLIC_IP['ipv6']:
                 routing_to_vpn_client('ipv6', port, protocol)
-    print(
-        f"{cmd('iptables --list')}"
-        f"{cmd('ip6tables --list')}"
-    )
+
+    cmd('iptables --list')
+    cmd('ip6tables --list')
